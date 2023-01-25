056f6465f65e979e454061840ff4afa7a8d9e454
Evgeniy Oshepkov <antarievg@yandex.ru>
2023-01-25 15:00:46 +0300

    fixed

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,6 +1,6 @@
 export const COUNT_PAGINATION = 3;
 export const DATA = {};
-export const API_URL = 'http://localhost:8024';
+export const API_URL = 'https://typical-lydian-risk.glitch.me';
 export const TITLE = {
   women: {
     title: 'Новая коллекция Бюстгальтер-балконет ',
